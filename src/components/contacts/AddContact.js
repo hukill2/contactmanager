f995c1aaef760d74aa7aa8054755e521d9bf6bde
Forest Hukill <forest@foresthukill.com>
2019-01-14 09:53:46 -0800

    added some fuc to addcontacts

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -7,13 +7,19 @@ class AddContact extends Component {
     phone: ""
   };
 
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
     const { name, email, phone } = this.state;
     return (
       <div className="card mb-3">
         <div className="card-header">Add Contact</div>
         <div className="card-body">
-          <form>
+          <form onSubmit={this.onSubmit}>
             <div className="form-group">
               <label htmlFor="name">Name</label>
               <input
@@ -22,6 +28,7 @@ class AddContact extends Component {
                 placeholder="Enter Name..."
                 name="name"
                 value={name}
+                onChange={this.onChange}
               />
             </div>
             <div className="form-group">
@@ -32,6 +39,7 @@ class AddContact extends Component {
                 placeholder="Enter Email..."
                 name="email"
                 value={email}
+                onChange={this.onChange}
               />
             </div>
             <div className="form-group">
@@ -42,6 +50,7 @@ class AddContact extends Component {
                 placeholder="Enter Phone..."
                 name="phone"
                 value={phone}
+                onChange={this.onChange}
               />
             </div>
             <input
