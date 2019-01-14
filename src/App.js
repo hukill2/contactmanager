b3972c8dc75109e3a7051b1444817c8889852142
Forest Hukill <forest@foresthukill.com>
2019-01-14 14:35:59 -0800

    added redirects

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import Contacts from "./components/contacts/Contacts";
 import Header from "./components/layout/Header";
 import About from "./components/pages/about";
 import AddContact from "./components/contacts/AddContact";
-import { Provider } from "./context";
+import NotFound from "./components/pages/NotFound";
 
+import { Provider } from "./context";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -22,6 +23,7 @@ class App extends Component {
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/contact/add" component={AddContact} />
                 <Route exact path="/about" component={About} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
