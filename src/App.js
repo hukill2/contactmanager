ec64d4a4e2cccaf9b72ece519e272a82640ddb03
Forest Hukill <forest@foresthukill.com>
2019-01-13 20:40:22 -0800

    proptypes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header branding="Contact Manager"/>
+        <Header />
         <Contact name="John Doe"
         email="jdoe@gmail.com" phone="555-555-5555"/>
         <Contact name="Karen Smith"
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Contact extends Component {
-  render() {
-      const { name, email, phone } = this.props;
-    return (
-      <div>
-        <h4>{name}</h4>
-        <ul>
-            <li>{email}</li>
-            <li>{phone}</li>
-        </ul>
-      </div>
-    )
-  }
+	render() {
+		const { name, email, phone } = this.props;
+		return (
+			<div>
+				<h4>{name}</h4>
+				<ul>
+					<li>{email}</li>
+					<li>{phone}</li>
+				</ul>
+			</div>
+		);
+	}
 }
+
+Contact.PropTypes = {
+	name: PropTypes.string.isRequired,
+	email: PropTypes.string.isRequired,
+	phone: PropTypes.string.isRequired
+};
 
 export default Contact;
