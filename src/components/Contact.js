28564836ef2d4dcb1091eefab110a9ad14b5c8f4
Forest Hukill <forest@foresthukill.com>
2019-01-14 08:51:07 -0800

    delete function

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,10 @@ class Contact extends Component {
 		showContactInfo: false
 	};
 
+	onDeleteClick = () => {
+		this.props.deleteClickHandler();
+	};
+
 	render() {
 		const { name, email, phone } = this.props.contact;
 		const { showContactInfo } = this.state;
@@ -20,6 +24,12 @@ class Contact extends Component {
 							})
 						}
 						className="fas fa-sort-down"
+						style={{ cursor: "pointer" }}
+					/>
+					<i
+						className="fas fa-times"
+						style={{ cursor: "pointer", float: "right", color: "red" }}
+						onClick={this.onDeleteClick}
 					/>
 				</h4>
 				{showContactInfo ? (
@@ -34,7 +44,8 @@ class Contact extends Component {
 }
 
 Contact.propTypes = {
-	contact: PropTypes.object.isRequired
+	contact: PropTypes.object.isRequired,
+	deleteClickHandler: PropTypes.func.isRequired
 };
 
 export default Contact;
